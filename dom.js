692bdf13005db2b582406989f011d0f62a18f9c1
MOHD ASIF <89849174+asifamualig@users.noreply.github.com>
2023-12-12 00:51:28 +0530

    uploaded files. list item 2 have green background color, and all other elements have bold color font

---
+++ dom.js
@@ -0,0 +1,14 @@
+//examining the document object
+
+//console.dir(document);
+        var Headertitle = document.getElementById('header-title');
+        Headertitle.textContent = 'HELLO  ASIF';
+        Headertitle.style.border ='solid 3px black';
+
+        var titles = document.querySelectorAll('.title'); 
+        titles[0].style.border='solid green';
+        titles[0].style.backgroundColor='green';    
+
+
+
+        
